b4624f7c8f007a49561aa5e254b2850a06637b88
aaron-kristopher <aaron.lim.cstr@gmail.com>
2023-02-19 12:08:27 +0800

    Added comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class App {
+public class Calculator {
     static Scanner scanner = new Scanner(System.in);
     static String run;
 
@@ -19,10 +19,15 @@ public class App {
 
         String notation = getNotation();
 
+        // Splits by mathematical operation & returns only numbers
+        // Splits by numbers of varying digits & returns only operations
+
         String[] stringNumbers = notation.split("[e\\*\\/+-]");
         String[] operationsArray = notation.split("([0-9]+)");
 
         convertToNumbers(numbers, stringNumbers);
+
+        // Converts operations array to arrayList for easier manipulation
         Collections.addAll(operations, operationsArray);
 
         System.out.printf("Answer: %s", doEquation(numbers, operations, answer));
@@ -33,6 +38,8 @@ public class App {
         return scanner.nextLine();
     }
 
+    // Converts string numbers to double for future calculation
+
     static void convertToNumbers(ArrayList<Double> numbers, String[] stringNumbers) {
         for (String number : stringNumbers) {
             numbers.add((double) Integer.parseInt(number));
@@ -41,9 +48,13 @@ public class App {
 
     static double doEquation(ArrayList<Double> numbers, ArrayList<String> operations, double answer) {
 
-        if (operations.contains("e")) {
+        if (operations.contains("e")) { // Checks for exponent denoted by symbol 'e'
 
             for (int i = 0; i < operations.size(); i++) {
+
+                // Gets numbers on both sides of the operation to do calculation
+                // The numbers to be calculated will always precede and succeed the operation
+                // Hence accessing them by operation index
 
                 if (operations.get(i).equals("e")) {
                     answer = Math.pow(numbers.get(i - 1),
@@ -51,11 +62,23 @@ public class App {
                     operations.remove(i);
                     numbers.remove(i);
                     numbers.set(i - 1, answer);
+
+                    // Recurses to ensure all instances of the operaiton are done before
+                    // following the next order of operation
+
+                    if (operations.size() != 1)
+                        return doEquation(numbers, operations, answer);
                 }
             }
         }
 
+        // Catches either operation and iterates over operations array
+        // to follow operation rule precedence
+
         if (operations.contains("*") || operations.contains("/")) {
+
+            // Ensures that since both operations are of equal value,
+            // whichever operation comes first is evaluated
 
             for (int i = 0; i < operations.size(); i++) {
 
