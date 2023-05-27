aadbf786db2ffe19906ca23c88295ac86c21d037
aaron-kristopher <aaron.lim.cstr@gmail.com>
2023-05-27 19:40:09 +0800

    Modified files for jsp conversion

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,246 +1,142 @@
 import java.util.*;
 
 public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-    static String run;
 
-    public static void main(String[] args) {
-        do {
-            runCalculator();
-        } while (isRerun());
+    private static String calculate(String equation) {
 
-    }
+        ArrayList<String> infix = convertToInfix(equation);
 
-    static void runCalculator() {
-        double answer = 0.0;
+        ArrayList<String> postfix = infixToPostfix(infix);
 
-        String notation = getNotation();
-
-        // Splits by mathematical operation & returns only numbers
-        // Splits by numbers of varying digits & returns only operations
-
-        String[] tokens = notation.split("\\s+");
-
-        // String[] stringNumbers = notation.split("[\\(\\)\\^\\*\\/+-]");
-
-        ArrayList<Double> numbers = parseNumbers(tokens);
-        ArrayList<String> operations = parseMathOperations(tokens);
-
-        // Converts operations array to arrayList for easier manipulation
-
-        String newNotation = displayNotation(numbers, operations);
-
-        System.out.printf("\n\nAnswer: %s", calculate(numbers, operations, newNotation, answer));
-
-    }
-
-    private static ArrayList<String> parseMathOperations(String[] tokens) {
-        ArrayList<String> operations = new ArrayList<>();
-
-        for (String token : tokens) {
-            if (token.matches("[\\(\\)\\^+\\-*/]")) {
-                operations.add(token);
-            }
+        System.out.print("Postfix Equivalence: ");
+        for (String postfixElement : postfix) {
+            System.out.print(postfixElement + " ");
         }
-
-        return operations;
+        return evaluatePostfix(postfix);
     }
 
-    private static ArrayList<Double> parseNumbers(String[] tokens) {
-        ArrayList<Double> numbers = new ArrayList<>();
+    private static ArrayList<String> convertToInfix(String equation) {
+        ArrayList<String> infix = new ArrayList<>();
+        for (int i = 0; i < equation.length(); i++) {
 
-        for (String token : tokens) {
-            if (token.matches("([0-9]+)")) {
-                if (!token.equals(""))
-                    numbers.add((double) Integer.parseInt(token));
-            }
-        }
+            char equationCharacter = equation.charAt(i);
 
-        return numbers;
-    }
+            if (Character.isDigit(equationCharacter) || equationCharacter == '.') {
 
-    static String getNotation() {
-        System.out.print("\nEnter notation: ");
-        return scanner.nextLine();
-    }
+                int digitCharacter = i;
 
-    // Converts string numbers to double for future calculation
+                while (digitCharacter < equation.length() && (Character.isDigit(equation.charAt(digitCharacter))
+                        || equation.charAt(digitCharacter) == '.')) {
 
-    // static void convertToNumbers(ArrayList<Double> numbers, String[]
-    // stringNumbers) {
-    // for (String number : stringNumbers) {
-    // if (!number.equals(""))
-    // numbers.add((double) Integer.parseInt(number));
-    // }
-    // }
-
-    static String displayNotation(ArrayList<Double> numbers, ArrayList<String> operations) {
-
-        return "Numbers: " + numbers + "\nOperations: " + operations;
-    }
-
-    static double calculate(ArrayList<Double> numbers, ArrayList<String> operations, String notation, double answer) {
-
-        if (operations.contains("(")) {
-
-            int parenthesisCount = 0;
-
-            for (String operation : operations) {
-                if (operation.equals("("))
-                    parenthesisCount++;
-            }
-
-            int subsetStart = operations.lastIndexOf("(");
-            int subsetEnd = operations.indexOf(")");
-
-            ArrayList<String> operationSublist = new ArrayList<>();
-            operationSublist.addAll(operations.subList(subsetStart + 1, subsetEnd));
-            ArrayList<Double> numberSublist = new ArrayList<>();
-            numberSublist.addAll(numbers.subList(subsetStart - (parenthesisCount - 1), subsetEnd));
-
-            for (int i = subsetStart; i <= subsetEnd; i++) {
-                if (i != subsetEnd)
-                    numbers.remove(subsetStart - (parenthesisCount - 1));
-                operations.remove(subsetStart);
-            }
-
-            numbers.add(subsetStart - (parenthesisCount - 1),
-                    calculate(numberSublist, operationSublist, notation, answer));
-
-            return calculate(numbers, operations, notation, answer);
-
-        }
-
-        if (operations.contains("^")) { // Checks for exponent denoted by symbol '^'
-
-            for (int i = 0; i < operations.size(); i++) {
-
-                // Gets numbers on both sides of the operation to do calculation
-                // The numbers to be calculated will always precede and succeed the operation
-                // Hence accessing them by operation index
-
-                if (operations.get(i).equals("^")) {
-
-                    double numBefore = numbers.get(i);
-                    double numAfter = numbers.get(i + 1);
-
-                    answer = Math.pow(numBefore, numAfter);
-
-                    System.out.printf("Exponents: %s ^ %s = %s\n\n", numBefore, numAfter, answer);
-
-                    numbers.set(i, answer);
-                    operations.remove(i);
-                    numbers.remove(i + 1);
-
-                    String newNotation = displayNotation(numbers, operations);
-
-                    // Recurses to ensure all instances of the operaiton are done before
-                    // following the next order of operation
-
-                    if (numbers.size() != 0)
-                        return calculate(numbers, operations, newNotation, answer);
+                    digitCharacter++;
                 }
+
+                infix.add(equation.substring(i, digitCharacter));
+                i = digitCharacter - 1;
+
+            } else {
+                infix.add(Character.toString(equationCharacter));
             }
         }
-
-        // Catches either operation and iterates over operations array
-        // to follow operation rule precedence
-
-        if (operations.contains("*") || operations.contains("/")) {
-
-            // Ensures that since both operations are of equal value,
-            // whichever operation comes first is evaluated
-
-            for (int i = 0; i < operations.size(); i++) {
-
-                if (operations.get(i).equals("*")) {
-
-                    double numBefore = numbers.get(i);
-                    double numAfter = numbers.get(i + 1);
-
-                    answer = numbers.get(i) * numbers.get(i + 1);
-
-                    System.out.printf("Multiplication: %s * %s = %s\n\n", numBefore, numAfter, answer);
-
-                    numbers.set(i, answer);
-                    operations.remove(i);
-                    numbers.remove(i + 1);
-
-                    String newNotation = displayNotation(numbers, operations);
-
-                    if (numbers.size() != 0)
-                        return calculate(numbers, operations, newNotation, answer);
-
-                } else if (operations.get(i).equals("/")) {
-
-                    double numBefore = numbers.get(i);
-                    double numAfter = numbers.get(i + 1);
-
-                    answer = numbers.get(i) / numbers.get(i + 1);
-
-                    System.out.printf("Division: %s / %s = %s\n\n", numBefore, numAfter, answer);
-
-                    numbers.set(i, answer);
-                    operations.remove(i);
-                    numbers.remove(i + 1);
-
-                    String newNotation = displayNotation(numbers, operations);
-
-                    if (numbers.size() != 0)
-                        return calculate(numbers, operations, newNotation, answer);
-                }
-            }
-        }
-
-        if (operations.contains("+") || operations.contains("-")) {
-
-            for (int i = 0; i < operations.size(); i++) {
-
-                if (operations.get(i).equals("+")) {
-
-                    double numBefore = numbers.get(i);
-                    double numAfter = numbers.get(i + 1);
-
-                    answer = numbers.get(i) + numbers.get(i + 1);
-
-                    System.out.printf("Addition: %s + %s = %s\n\n", numBefore, numAfter, answer);
-
-                    numbers.set(i, answer);
-                    operations.remove(i);
-                    numbers.remove(i + 1);
-
-                    String newNotation = displayNotation(numbers, operations);
-
-                    if (numbers.size() != 0)
-                        return calculate(numbers, operations, newNotation, answer);
-
-                } else if (operations.get(i).equals("-")) {
-
-                    double numBefore = numbers.get(i);
-                    double numAfter = numbers.get(i + 1);
-
-                    answer = numbers.get(i) - numbers.get(i + 1);
-
-                    System.out.printf("Subtraction: %s - %s = %s\n\n", numBefore, numAfter, answer);
-
-                    numbers.set(i, answer);
-                    operations.remove(i);
-                    numbers.remove(i + 1);
-
-                    String newNotation = displayNotation(numbers, operations);
-
-                    if (numbers.size() != 0)
-                        return calculate(numbers, operations, newNotation, answer);
-                }
-            }
-        }
-
-        return answer;
+        return infix;
     }
 
-    static boolean isRerun() {
-        System.out.print("\nDo you want to do another calculation? (Y/N): ");
-        run = scanner.nextLine().toLowerCase();
-        return run.equals("y");
+    public static ArrayList<String> infixToPostfix(ArrayList<String> infix) {
+        ArrayList<String> postfix = new ArrayList<>();
+        ArrayList<String> operationArray = new ArrayList<>();
+
+        for (int i = 0; i < infix.size(); i++) {
+            String infixElement = infix.get(i);
+
+            if (isNumber(infixElement)) {
+                postfix.add(infixElement);
+
+            } else if (infixElement.equals("(")) {
+                operationArray.add(infixElement);
+
+            } else if (infixElement.equals(")")) {
+
+                for (int operationIndex = operationArray.size() - 1; operationIndex >= 0; operationIndex--) {
+
+                    String operation = operationArray.get(operationIndex);
+
+                    if (operation.equals("(")) {
+                        operationArray.remove(operationIndex);
+                        break;
+
+                    }
+                    postfix.add(operation);
+                    operationArray.remove(operationIndex);
+                }
+
+            } else {
+                for (int operationIndex = operationArray.size() - 1; operationIndex >= 0; operationIndex--) {
+
+                    String operation = operationArray.get(operationIndex);
+
+                    if (operation.equals("(") || order(operation) < order(infixElement)) {
+                        break;
+                    }
+                    postfix.add(operation);
+                    operationArray.remove(operationIndex);
+                }
+                operationArray.add(infixElement);
+            }
+        }
+
+        for (int operation = operationArray.size() - 1; operation >= 0; operation--) {
+            postfix.add(operationArray.get(operation));
+            operationArray.remove(operation);
+        }
+
+        return postfix;
+    }
+
+    static String evaluatePostfix(ArrayList<String> postfix) {
+        ArrayList<Double> answer = new ArrayList<>();
+
+        for (String postfixElement : postfix) {
+            if (isNumber(postfixElement)) {
+                answer.add(Double.parseDouble(postfixElement));
+                System.out.println("Updated List: " + answer.toString());
+            } else {
+                double secondNum = answer.remove(answer.size() - 1);
+                double firstNum = answer.remove(answer.size() - 1);
+                double result = 0;
+                if (postfixElement.equals("+")) {
+                    result = firstNum + secondNum;
+                } else if (postfixElement.equals("-")) {
+                    result = firstNum - secondNum;
+                } else if (postfixElement.equals("*")) {
+                    result = firstNum * secondNum;
+                } else if (postfixElement.equals("/")) {
+                    result = firstNum / secondNum;
+                } else if (postfixElement.equals("^")) {
+                    result = Math.pow(firstNum, secondNum);
+
+                }
+                answer.add(result);
+
+            }
+        }
+
+        return Double.toString(answer.get(0));
+    }
+
+    static boolean isNumber(String s) {
+        return s.matches("\\d+(\\.\\d+)?");
+    }
+
+    static int order(String s) {
+        if (s.equals("+") || s.equals("-")) {
+            return 1;
+        } else if (s.equals("*") || s.equals("/")) {
+            return 2;
+        } else if (s.equals("^")) {
+            return 3;
+        } else {
+            return -1;
+        }
+
     }
 }
